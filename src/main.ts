e4a4d109208a1615cbca2047749cc9ed04bf8b6f
crays26 <dinhkiet2610@gmail.com>
2025-08-13 15:31:45 +0700

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     transform: true
   }));
+  app.use(cookieParser());
   await app.listen(process.env.APP_PORT ?? 3000);
 }
 bootstrap();
