1e22323d0b9671ff7c603153d87cfe3f64266a23
crays26 <dinhkiet2610@gmail.com>
2025-08-13 15:37:25 +0700

    add account.module.ts

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -4,16 +4,34 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { AuthController } from './presentation/controllers/account.controller';
 import { AccountRepository } from './infrastructure/repositories/account.repo';
 import { EntityManager } from '@mikro-orm/postgresql';
-import { Book } from './infrastructure/entities/book.entity';
+import { Role } from './infrastructure/entities/role.entity';
 import { SignUpAccountCommandHandler } from './application/commands/sign-up-account/handler';
+import { LogInAccountCommandHandler } from './application/commands/log-in-account/handler';
+import { AuthService } from 'src/shared/auth/auth.service';
+import { ShareAuthModule } from 'src/shared/auth/auth.module';
+import { CqrsModule } from '@nestjs/cqrs';
+import { GetAccountOfCurrentUserQueryHandler } from './application/queries/get-account-of-current-user/handler';
+
+const CommandHandlers = [
+  SignUpAccountCommandHandler,
+  LogInAccountCommandHandler,
+];
+
+const QueryHandlers = [GetAccountOfCurrentUserQueryHandler];
 @Module({
-  imports: [MikroOrmModule.forFeature([Account, Book])],
+  imports: [
+    MikroOrmModule.forFeature([Account, Role]),
+    ShareAuthModule,
+    CqrsModule,
+  ],
 
   controllers: [AuthController],
   providers: [
-       AccountRepository,
-       SignUpAccountCommandHandler
-    ],
+    AccountRepository,
+    AuthService,
+    ...CommandHandlers,
+    ...QueryHandlers,
+  ],
   exports: [],
 })
 export class AccountModule {}
