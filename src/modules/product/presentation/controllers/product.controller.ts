4ba627e6d1f7bec4a93500e4a0d15f374637ee2e
crays26 <dinhkiet2610@gmail.com>
2025-09-03 21:07:12 +0700

    add get endpoints to find product by Id

--- src/modules/product/presentation/controllers/product.controller.ts
+++ src/modules/product/presentation/controllers/product.controller.ts
@@ -1,23 +1,34 @@
-import { Controller, Param, Post, Put } from '@nestjs/common';
-import { CommandBus } from '@nestjs/cqrs';
+import { Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { ProductCreateDto } from '../dtos/requests/product.create.dto';
 import { Body } from '@nestjs/common';
 import { CreateProductCommand } from '../../application/commands/create-product/command';
 import { ProductUpdateDto } from '../dtos/requests/product.update.dto';
 import { UpdateProductCommand } from '../../application/commands/update-product/command';
+import { GetProductByIdQuery } from '../../application/queries/queries/get-product-by-id/query';
+import { ProductDto } from '../dtos/responses/product.dto';
 
 @Controller('products')
 export class ProductController {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
 
   @Post()
-  async createProduct(@Body() body: ProductCreateDto) {
+  async createProduct(@Body() body: ProductCreateDto): Promise<string> {
     const command = new CreateProductCommand(body);
     return await this.commandBus.execute(command);
   }
 
-  @Put('/:productId')
-  async updateProduct(@Param('productId') productId: string, @Body() body: ProductUpdateDto) {
+  @Get(':productId')
+  async findProductById(@Param('productId') productId: string): Promise<ProductDto> {
+    const query = new GetProductByIdQuery(productId);
+    return await this.queryBus.execute(query)
+  }
+
+  @Put(':productId')
+  async updateProduct(@Param('productId') productId: string, @Body() body: ProductUpdateDto): Promise<string | null> {
     const command = new UpdateProductCommand({ ...body, id: productId });
     return await this.commandBus.execute(command);
   }
