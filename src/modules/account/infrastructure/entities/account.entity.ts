4e9b5fd36befa48de4dbc41ac14cc43a2c5939e0
crays26 <dinhkiet2610@gmail.com>
2025-09-07 21:29:28 +0700

    add one-to-one relationship with Vendor in Account module

--- src/modules/account/infrastructure/entities/account.entity.ts
+++ src/modules/account/infrastructure/entities/account.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, ManyToMany, PrimaryKey, Property, Unique } from '@mikro-orm/core';
+import { Entity, ManyToMany, OneToOne, PrimaryKey, Property, Unique } from '@mikro-orm/core';
 import { Role } from './role.entity';
+import { VendorEntity } from 'src/modules/vendor/infrastructure/entities/vendor.entity';
 import { Collection } from '@mikro-orm/core';
 
 @Entity({ tableName: 'account' })
@@ -18,6 +19,9 @@ export class Account {
   
    @Property()
    password!: string;
+
+   @OneToOne(() => VendorEntity, vendor => vendor.account, { nullable: true })
+   vendor?: VendorEntity;
 
    @ManyToMany(() => Role, role => role.accounts, { owner: true }) 
    roles = new Collection<Role>(this);
