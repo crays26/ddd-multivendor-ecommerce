32624874a9816d17e3b98a531eae861f5f381db2
crays26 <dinhkiet2610@gmail.com>
2025-09-07 21:30:13 +0700

    add aggregate root in domain in Vendor module

---
+++ src/modules/vendor/domain/aggregate-roots/vendor.agg-root.ts
@@ -0,0 +1,75 @@
+import { BadRequestException } from '@nestjs/common';
+import { BaseAggregateRoot } from 'src/shared/ddd/domain/base/BaseAggregateRoot';
+import { v4 } from 'uuid';
+
+
+interface VendorProps {
+  id: string;
+  name: string;
+  description: string;
+  accountId: string;
+  rating: number;
+}
+
+interface CreateVendorProps {
+  id?: string;
+  name: string;
+  description: string;
+  accountId: string;
+}
+
+export class VendorAggRoot extends BaseAggregateRoot<
+  string,
+  VendorProps
+> {
+  private constructor(props: VendorProps) {
+    super(props);
+  }
+
+  static create(props: CreateVendorProps): VendorAggRoot {
+    return new VendorAggRoot({
+      id: props.id ? props.id : v4(),
+      name: props.name,
+      description: props.description,
+      accountId: props.accountId,
+      rating: 0,
+    });
+  }
+
+//   static rehydrate(props: AccountProps): AccountDomainEntity {
+//     return new AccountDomainEntity(props);
+//   }
+
+
+  public getUsername(): string {
+    return this.props.name;
+  }
+
+  public getDescription(): string {
+    return this.props.description;
+  }
+
+  public getAccountId(): string {
+    return this.props.accountId;
+  }
+
+  public getRating(): number {
+    return this.props.rating;
+  }
+
+
+  public setName(newName: string): void {
+    if (!newName || newName.length < 4) {
+      throw new BadRequestException('Username must be at least 4 characters');
+    }
+    this.props.name = newName;
+  }
+
+  public setDescription(newDescription: string): void {
+    this.props.description = newDescription;
+  }
+
+  public setRating(newRating: number): void {
+    this.props.rating = newRating;
+  }
+}
