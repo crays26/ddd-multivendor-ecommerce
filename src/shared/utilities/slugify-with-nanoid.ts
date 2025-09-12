0d1c214ee5e8a0a90d9cbed16bed8871fba22af2
crays26 <dinhkiet2610@gmail.com>
2025-09-12 22:13:34 +0700

    update slugifyWithNanoid function in utilities

--- src/shared/utilities/slugify-with-nanoid.ts
+++ src/shared/utilities/slugify-with-nanoid.ts
@@ -1,6 +1,8 @@
 import slugify from "slugify";
 import { nanoid } from "nanoid";
 
-export function slugifyWithNanoid(string: string, postfixLength: number) {
+export const NANOID_POSTFIX_LENGTH = 8;
+
+export function slugifyWithNanoid(string: string, postfixLength: number = NANOID_POSTFIX_LENGTH) {
     return `${slugify(string)}-${nanoid(postfixLength)}`;
 }
