0085a4dc4451598a013b98cc85d1f7597e308e2d
crays26 <dinhkiet2610@gmail.com>
2025-09-30 16:59:23 +0700

    register CqrsModule as a global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,14 +11,15 @@ import { ProductModule } from './modules/product/product.module';
 import { VendorModule } from './modules/vendor/vendor.module';
 import { ShareCacheModule } from './shared/ddd/infrastructure/cache/cache.module';
 import { ShareDistributedLockModule } from './shared/ddd/infrastructure/distributed-lock/distributed-lock.module';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     MikroOrmModule.forRoot(mikroConfig),
+    CqrsModule.forRoot(),
     AccountModule,
     ProductModule,
     CustomerModule,
