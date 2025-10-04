1134eeb6f51c766ceae71bc9296082e86e9c028a
crays26 <dinhkiet2610@gmail.com>
2025-10-04 21:51:05 +0700

    change save method into 2 seperate: insert and update in product repo in product module

--- src/modules/product/infrastructure/repositories/product.repo.ts
+++ src/modules/product/infrastructure/repositories/product.repo.ts
@@ -15,6 +15,92 @@ export class ProductRepository {
     private readonly em: EntityManager,
   ) {}
 
+  async insert(domain: ProductAggRoot): Promise<void> {
+    const product = new ProductEntity();
+    product.id = domain.getId();
+    product.name = domain.getName();
+    product.slug = domain.getSlug();
+
+    for (const v of domain.getVariants()) {
+      const variant = new ProductVariantEntity();
+      variant.id = v.getId();
+      variant.isBase = false;
+      variant.isSoftDeleted = false;
+      variant.name = v.getName();
+      variant.price = v.getPrice();
+      variant.skuCode = v.getSkuCode();
+      variant.stock = v.getStock();
+      variant.associatedAttributes = v.getAssociatedAttributes();
+      product.variants.add(variant);
+
+      for (const a of domain.getAttributes()) {
+        const attribute = new ProductAttributeEntity();
+        attribute.id = a.getId();
+        attribute.isSoftDeleted = false;
+        attribute.key = a.getKey();
+        attribute.values = a.getValues();
+        product.attributes.add(attribute);
+      }
+
+      this.em.persist(product);
+    }
+  }
+
+  async update(domain: ProductAggRoot): Promise<void> {
+    const product: ProductEntity = await this.repo.findOneOrFail(
+      { id: domain.getId() },
+      { populate: ['variants', 'attributes'] },
+    );
+
+    product.name = domain.getName();
+    product.slug = domain.getSlug();
+
+    const existingVariants = product.variants.getItems();
+
+    const domainVariantIdList = domain.getVariants().map((v) => v.getId());
+    existingVariants
+      .filter((v) => !domainVariantIdList.includes(v.id))
+      .forEach((v) => (v.isSoftDeleted = true));
+
+    for (const v of domain.getVariants()) {
+      let variant = existingVariants.find((ev) => ev.id === v.getId());
+      if (!variant) {
+        variant = new ProductVariantEntity();
+        variant.id = v.getId();
+        variant.isBase = false;
+        variant.isSoftDeleted = false;
+
+        product.variants.add(variant);
+      }
+      variant.name = v.getName();
+      variant.price = v.getPrice();
+      variant.skuCode = v.getSkuCode();
+      variant.stock = v.getStock();
+      variant.associatedAttributes = v.getAssociatedAttributes();
+    }
+
+    const existingAttributes = product.attributes.getItems();
+
+    const domainAttributeIdList = domain.getAttributes().map((a) => a.getId());
+    existingAttributes
+      .filter((a) => !domainAttributeIdList.includes(a.id))
+      .forEach((attr) => (attr.isSoftDeleted = true));
+
+    for (const a of domain.getAttributes()) {
+      let attribute = existingAttributes.find((ea) => ea.id === a.getId());
+      if (!attribute) {
+        attribute = new ProductAttributeEntity();
+        attribute.id = a.getId();
+        attribute.isSoftDeleted = false;
+        product.attributes.add(attribute);
+      }
+      attribute.key = a.getKey();
+      attribute.values = a.getValues();
+    }
+
+    this.em.persist(product);
+  }
+
   async save(domain: ProductAggRoot): Promise<void> {
     let product: ProductEntity | null = await this.repo.findOne(
       { id: domain.getId() },
@@ -49,7 +135,6 @@ export class ProductRepository {
       variant.stock = v.getStock();
       variant.associatedAttributes = v.getAssociatedAttributes();
     }
-
 
     const existingAttributes = product.attributes.getItems();
 
