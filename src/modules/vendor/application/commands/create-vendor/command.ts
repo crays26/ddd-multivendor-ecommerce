c0a5627536e0d03a4343e939272d52f70b18bef9
crays26 <dinhkiet2610@gmail.com>
2025-10-05 20:06:03 +0700

    update params in CreateVendorCommand in application in vendor module

--- src/modules/vendor/application/commands/create-vendor/command.ts
+++ src/modules/vendor/application/commands/create-vendor/command.ts
@@ -2,7 +2,9 @@ import { Command } from '@nestjs/cqrs';
 import { VendorCreateDto } from 'src/modules/vendor/presentation/dtos/requests/vendor.create.dto';
 
 export class CreateVendorCommand extends Command<string> {
-  constructor(public readonly payload: VendorCreateDto) {
+  constructor(
+    public readonly payload: VendorCreateDto & { accountId: string },
+  ) {
     super();
   }
 }
