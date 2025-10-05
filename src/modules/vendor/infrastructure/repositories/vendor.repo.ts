2d4e34a6e66b7a4d6ecd5bb4a5aca6b01c3a45a2
crays26 <dinhkiet2610@gmail.com>
2025-10-05 19:53:28 +0700

    update vendor repo in infrastructure in vendor module

--- src/modules/vendor/infrastructure/repositories/vendor.repo.ts
+++ src/modules/vendor/infrastructure/repositories/vendor.repo.ts
@@ -9,25 +9,16 @@ import { Account } from '../../../account/infrastructure/entities/account.entity
 @Injectable()
 export class VendorRepository {
   constructor(
-    @InjectRepository(VendorEntity)
-    private readonly repo: EntityRepository<VendorEntity>,
     private readonly em: EntityManager,
   ) {}
 
-  async save(domain: VendorAggRoot): Promise<void> {
-    let vendor: VendorEntity | null = await this.repo.findOne(
-      { id: domain.getId() },
-      { populate: ['account'] },
-    );
-    if (!vendor) {
-      vendor = new VendorEntity();
-      vendor.id = domain.getId();
-    }
-
+  async insert(domain: VendorAggRoot): Promise<void> {
+    const vendor = new VendorEntity();
+    vendor.id = domain.getId();
     vendor.name = domain.getName();
     vendor.description = domain.getDescription();
     vendor.account = this.em.getReference(Account, domain.getAccountId());
 
-    await this.em.persistAndFlush(vendor);
+    this.em.persist(vendor);
   }
 }
