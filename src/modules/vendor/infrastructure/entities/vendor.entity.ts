abf5eb21323b44e6756c86ab800c3dd8675fbce0
crays26 <dinhkiet2610@gmail.com>
2025-10-07 21:39:55 +0700

    add product collection in vendor persistence entity in vendor module

--- src/modules/vendor/infrastructure/entities/vendor.entity.ts
+++ src/modules/vendor/infrastructure/entities/vendor.entity.ts
@@ -1,26 +1,37 @@
-import { Entity, OneToOne, PrimaryKey, Property, Unique } from '@mikro-orm/core';
+import {
+  Collection,
+  Entity,
+  OneToMany,
+  OneToOne,
+  PrimaryKey,
+  Property,
+  Unique,
+} from '@mikro-orm/core';
 import { Account } from 'src/modules/account/infrastructure/entities/account.entity';
+import { ProductEntity } from 'src/modules/product/infrastructure/entities/Product.entity';
 
 @Entity({ tableName: 'vendor' })
 export class VendorEntity {
+  @PrimaryKey({ type: 'uuid' })
+  id!: string;
 
-   @PrimaryKey({ type: 'uuid' })
-   id!: string;
+  @Property()
+  @Unique()
+  name!: string;
 
-   @Property()
-   @Unique()
-   name!: string;
+  @Property()
+  @Unique()
+  slug!: string;
 
-   @Property()
-   @Unique()
-   slug!: string;
+  @Property({ type: 'text' })
+  description!: string;
 
-   @Property({ type: 'text' })
-   description!: string;
+  @Property({ type: 'double precision' })
+  rating!: number;
 
-   @Property({ type: 'double precision' })
-   rating!: number;
+  @OneToOne(() => Account, (account) => account.vendor, { owner: true })
+  account!: Account;
 
-   @OneToOne(() => Account, account => account.vendor, { owner: true })
-   account!: Account;
+  @OneToMany(() => ProductEntity, (product) => product.vendor)
+  products = new Collection<ProductEntity>(this);
 }
