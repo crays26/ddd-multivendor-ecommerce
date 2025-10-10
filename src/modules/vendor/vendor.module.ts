60149260092caf25b9510f83a4dc192860348edf
crays26 <dinhkiet2610@gmail.com>
2025-10-10 23:57:35 +0700

    modify vendor.module.ts in vendor module

--- src/modules/vendor/vendor.module.ts
+++ src/modules/vendor/vendor.module.ts
@@ -3,19 +3,20 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { CqrsModule } from '@nestjs/cqrs';
 import { VendorEntity } from './infrastructure/entities/vendor.entity';
 import { VendorRepository } from './infrastructure/repositories/vendor.repo';
+import { VendorReadRepository } from 'src/modules/vendor/infrastructure/repositories/vendor.read.repo';
+import { CreateVendorCommandHandler } from 'src/modules/vendor/application/commands/create-vendor/handler';
+import { GetVendorByAccountIdQueryHandler } from 'src/modules/vendor/application/queries/get-vendor-by-account-id/handler';
 
-const CommandHandlers = []
-const QueryHandlers = []
+const CommandHandlers = [CreateVendorCommandHandler];
+const QueryHandlers = [GetVendorByAccountIdQueryHandler];
 
 @Module({
-  imports: [
-    MikroOrmModule.forFeature([VendorEntity]),
-    CqrsModule,
-  ],
+  imports: [MikroOrmModule.forFeature([VendorEntity]), CqrsModule],
 
   controllers: [],
   providers: [
     VendorRepository,
+    VendorReadRepository,
     ...CommandHandlers,
     ...QueryHandlers,
   ],
