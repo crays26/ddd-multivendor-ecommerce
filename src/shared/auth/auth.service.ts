92eb3f98eb828f225f39e1b5267b1a3c5a8c26bf
crays26 <dinhkiet2610@gmail.com>
2025-10-19 21:57:59 +0700

    add blacklist jwt token and blacklist checking methods in auth service

--- src/shared/auth/auth.service.ts
+++ src/shared/auth/auth.service.ts
@@ -1,19 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Response } from 'express';
 import { AuthPayload } from './types/auth-payload.type';
 import * as bcrypt from 'bcrypt';
-import {JwtTokenPair} from "src/shared/auth/types/jwt-token-pair.type";
-
+import { JwtTokenPair } from 'src/shared/auth/types/jwt-token-pair.type';
 const SALT_ROUND = 10;
-
+import { Cache } from '@nestjs/cache-manager';
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject('CACHE_MANAGER')
+    private readonly cache: Cache,
+  ) {}
 
   generateTokens(payload: AuthPayload): JwtTokenPair {
-    const accessToken = this.jwtService.sign(payload, { expiresIn: '15m', secret: process.env.ACCESS_TOKEN_SECRET_KEY! });
-    const refreshToken = this.jwtService.sign(payload, { expiresIn: '7d', secret: process.env.REFRESH_TOKEN_SECRET_KEY! });
+    const accessToken = this.jwtService.sign(payload, {
+      expiresIn: '15m',
+      secret: process.env.ACCESS_TOKEN_SECRET_KEY!,
+    });
+    const refreshToken = this.jwtService.sign(payload, {
+      expiresIn: '7d',
+      secret: process.env.REFRESH_TOKEN_SECRET_KEY!,
+    });
     return { accessToken, refreshToken };
   }
 
@@ -39,5 +48,26 @@ export class AuthService {
 
   async hash(string: string): Promise<string> {
     return await bcrypt.hash(string, SALT_ROUND);
+  }
+
+  async blacklistTokenPair(tokenPair: JwtTokenPair): Promise<void> {
+    const access = await this.jwtService.decode(tokenPair.accessToken);
+    const accessRemainingTime = access.exp - Math.floor(Date.now() / 1000);
+    const refresh = await this.jwtService.decode(tokenPair.refreshToken);
+    const refreshRemainingTime = refresh.exp - Math.floor(Date.now() / 1000);
+    await this.cache.set(
+      `blacklist:${tokenPair.accessToken}`,
+      '1',
+      accessRemainingTime,
+    );
+    await this.cache.set(
+      `blacklist:${tokenPair.refreshToken}`,
+      '1',
+      refreshRemainingTime,
+    );
+  }
+
+  async isTokenBlacklisted(token: string): Promise<boolean> {
+      return (await this.cache.get<string>(`blacklist:${token}`)) !== undefined;
   }
 }
