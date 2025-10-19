32464ab9c595df7b9a01d8d1c8ef94cefde8d0a0
crays26 <dinhkiet2610@gmail.com>
2025-10-19 21:59:32 +0700

    update jwt access and refresh strategy

--- src/shared/auth/strategies/jwt.access.strategy.ts
+++ src/shared/auth/strategies/jwt.access.strategy.ts
@@ -1,22 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AuthPayload } from '../types/auth-payload.type';
-
-
+import { Request } from 'express';
+import { AuthService } from 'src/shared/auth/auth.service';
 @Injectable()
-export class JwtAccessStrategy extends PassportStrategy(Strategy, 'jwt-access') {
-  constructor() {
+export class JwtAccessStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-access',
+) {
+  constructor(private readonly authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (req) => req?.cookies?.['access_token'],
       ]),
       ignoreExpiration: false,
-      secretOrKey: process.env.ACCESS_TOKEN_SECRET_KEY!
+      secretOrKey: process.env.ACCESS_TOKEN_SECRET_KEY!,
+      passReqToCallback: true,
     });
   }
 
-  async validate(payload: AuthPayload) {
+  async validate(req: Request, payload: AuthPayload) {
+    const token: string | undefined = req.cookies?.['access_token'];
+    if (!token) throw new UnauthorizedException('Missing access token');
+
+    const isBlacklisted = await this.authService.isTokenBlacklisted(token);
+    if (isBlacklisted) {
+      throw new UnauthorizedException('Access token has been blacklisted');
+    }
+
     return payload;
   }
 }
--- src/shared/auth/strategies/jwt.refresh.strategy.ts
+++ src/shared/auth/strategies/jwt.refresh.strategy.ts
@@ -1,22 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AuthPayload } from '../types/auth-payload.type';
+import { Request } from 'express';
 
+import { AuthService } from 'src/shared/auth/auth.service';
 
 @Injectable()
-export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
-  constructor() {
+export class JwtRefreshStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(private readonly authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (req) => req?.cookies?.['refresh_token'],
+        (req) => req?.cookies?.['access_token'],
       ]),
       ignoreExpiration: false,
-      secretOrKey: process.env.REFRESH_TOKEN_SECRET_KEY!,
+      secretOrKey: process.env.ACCESS_TOKEN_SECRET_KEY!,
+      passReqToCallback: true,
     });
   }
 
-  async validate(payload: AuthPayload) {
+  async validate(req: Request, payload: AuthPayload) {
+    const token: string | undefined = req.cookies?.['access_token'];
+    if (!token) throw new UnauthorizedException('Missing access token');
+
+    const isBlacklisted = await this.authService.isTokenBlacklisted(token);
+    if (isBlacklisted) {
+      throw new UnauthorizedException('Access token has been blacklisted');
+    }
+
     return payload;
   }
 }
