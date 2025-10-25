0aecc0e018d045ab5803685e2c58da67e4127411
crays26 <dinhkiet2610@gmail.com>
2025-10-25 22:51:09 +0700

    update command and handler of add-role-to-account in application in account module

--- src/modules/account/application/commands/add-role-to-account/command.ts
+++ src/modules/account/application/commands/add-role-to-account/command.ts
@@ -1,5 +1,5 @@
 import { Command } from '@nestjs/cqrs';
-import { Role } from 'src/shared/auth/enums/role.enum';
+
 
 export class AddRoleToAccountCommand extends Command<string> {
   constructor(
--- src/modules/account/application/commands/add-role-to-account/handler.ts
+++ src/modules/account/application/commands/add-role-to-account/handler.ts
@@ -1,19 +1,23 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { AddRoleToAccountCommand } from 'src/modules/account/application/commands/add-role-to-account/command';
-import { AccountRepository } from 'src/modules/account/infrastructure/repositories/account.repo';
 import {
   IUnitOfWork,
   UNIT_OF_WORK,
 } from 'src/shared/ddd/infrastructure/unit-of-work/unit-of-work.interface';
 import { Inject, NotFoundException } from '@nestjs/common';
 import { RoleIdVO } from 'src/modules/account/domain/value-objects/role-id.vo';
+import {
+  ACCOUNT_REPO,
+  IAccountRepository,
+} from 'src/modules/account/domain/repositories/account.repo.interface';
 
 @CommandHandler(AddRoleToAccountCommand)
 export class AddRoleToAccountCommandHandler
   implements ICommandHandler<AddRoleToAccountCommand>
 {
   constructor(
-    private readonly accountRepository: AccountRepository,
+    @Inject(ACCOUNT_REPO)
+    private readonly accountRepository: IAccountRepository,
     @Inject(UNIT_OF_WORK)
     private readonly uow: IUnitOfWork,
   ) {}
@@ -29,10 +33,13 @@ export class AddRoleToAccountCommandHandler
           `Account with id ${command.accountId} not found`,
         );
       accountAggRoot.addRole(RoleIdVO.create({ id: command.roleId }));
+
       await this.accountRepository.save(accountAggRoot);
+      await this.uow.commit();
       return `${command.roleId} added successfully`;
     } catch (error) {
       await this.uow.rollback();
+
       throw error;
     }
   }
