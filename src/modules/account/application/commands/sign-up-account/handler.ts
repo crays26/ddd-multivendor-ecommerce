16bf7a7540cd079b478865b0fb616d6530162d01
crays26 <dinhkiet2610@gmail.com>
2025-10-25 22:52:13 +0700

    inject IAccountRepository instead of AccountRepository in handler

--- src/modules/account/application/commands/sign-up-account/handler.ts
+++ src/modules/account/application/commands/sign-up-account/handler.ts
@@ -1,10 +1,14 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
 import { SignUpAccountCommand } from './command';
 import { AccountAggRoot } from 'src/modules/account/domain/aggregate-root/account.agg-root';
 import { AuthService } from 'src/shared/auth/auth.service';
 import { ConflictException, Inject } from '@nestjs/common';
-import { UNIT_OF_WORK, IUnitOfWork } from 'src/shared/ddd/infrastructure/unit-of-work/unit-of-work.interface';
+import {
+  UNIT_OF_WORK,
+  IUnitOfWork,
+} from 'src/shared/ddd/infrastructure/unit-of-work/unit-of-work.interface';
 import { AccountRepository } from 'src/modules/account/infrastructure/repositories/account.repo';
+import { AccountSignedUpEvent } from 'src/modules/account/domain/events/account-signed-up.event';
 
 @CommandHandler(SignUpAccountCommand)
 export class SignUpAccountCommandHandler
@@ -16,6 +20,7 @@ export class SignUpAccountCommandHandler
     // @Inject(ACCOUNT_REPO)
     // private readonly accountRepo: IAccountRepository,
     private readonly authService: AuthService,
+    private readonly eventPublisher: EventPublisher,
   ) {}
 
   async execute(command: SignUpAccountCommand) {
@@ -26,20 +31,21 @@ export class SignUpAccountCommandHandler
       if (accountExists)
         throw new ConflictException('This email has already been used');
 
-      const accountDomainEntity = AccountAggRoot.create(command.data);
+      const accountDomainEntity =
+        AccountAggRoot.create(command.data)
+
       const hashedPassword = await this.authService.hash(
         accountDomainEntity.getPassword(),
       );
       accountDomainEntity.setPassword(hashedPassword);
       await accountRepo.save(accountDomainEntity);
-      await this.unitOfWork.commit();
 
+      await this.unitOfWork.commit();
+      this.eventPublisher.mergeObjectContext(accountDomainEntity).commit();
       return `Account with id ${accountDomainEntity.getId()} registered successfully`;
     } catch (error) {
       await this.unitOfWork.rollback();
       throw error;
     }
-
-    
   }
 }
