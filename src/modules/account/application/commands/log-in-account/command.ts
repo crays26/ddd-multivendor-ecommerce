45388c060b3276c2f81c489f179f8f113a43f5d4
crays26 <dinhkiet2610@gmail.com>
2025-10-25 22:52:52 +0700

    inject IAccountRepository instead of AccountRepository in handler

--- src/modules/account/application/commands/log-in-account/command.ts
+++ src/modules/account/application/commands/log-in-account/command.ts
@@ -1,11 +1,8 @@
 import { Command } from '@nestjs/cqrs';
 import { LogInAccountDto } from 'src/modules/account/presentation/dtos/LogInAccount.dto';
-import { AuthPayload } from 'src/shared/auth/AuthPayload.interface';
+import {JwtTokenPair} from "src/shared/auth/types/jwt-token-pair.type";
 
-export class LogInAccountCommand extends Command<{
-  accessToken: string;
-  refreshToken: string;
-}> {
+export class LogInAccountCommand extends Command<JwtTokenPair> {
   constructor(
     public readonly data: LogInAccountDto
   ) {
--- src/modules/account/application/commands/log-in-account/handler.ts
+++ src/modules/account/application/commands/log-in-account/handler.ts
@@ -1,11 +1,12 @@
 import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
 import { LogInAccountCommand } from './command';
-import { AccountRepository } from 'src/modules/account/infrastructure/repositories/account.repo';
+import { IAccountRepository } from "src/modules/account/domain/repositories/account.repo.interface";
 import { ConflictException, Inject, NotFoundException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { AuthService } from 'src/shared/auth/auth.service';
-import { AuthPayload } from 'src/shared/auth/AuthPayload.interface';
+import { AuthPayload } from 'src/shared/auth/types/auth-payload.type';
 import { ACCOUNT_REPO } from 'src/modules/account/domain/repositories/account.repo.interface';
+import { JwtTokenPair } from 'src/shared/auth/types/jwt-token-pair.type';
 
 @CommandHandler(LogInAccountCommand)
 export class LogInAccountCommandHandler
@@ -13,14 +14,12 @@ export class LogInAccountCommandHandler
 {
   constructor(
     @Inject(ACCOUNT_REPO)
-    private readonly accountRepo: AccountRepository,
+    private readonly accountRepo: IAccountRepository,
     private readonly authService: AuthService,
     private readonly eventPublisher: EventPublisher,
   ) {}
 
-  async execute(
-    command: LogInAccountCommand,
-  ): Promise<{ accessToken: string; refreshToken: string }> {
+  async execute(command: LogInAccountCommand): Promise<JwtTokenPair> {
     const { email, password } = command.data;
     const accountDomain = await this.accountRepo.findByEmail(email);
     if (!accountDomain) throw new NotFoundException('Account does not exist');
@@ -32,7 +31,8 @@ export class LogInAccountCommandHandler
     if (!isPasswordValid)
       throw new ConflictException('Password does not match');
 
-    const accountWithContext = this.eventPublisher.mergeObjectContext(accountDomain);
+    const accountWithContext =
+      this.eventPublisher.mergeObjectContext(accountDomain);
     accountWithContext.logIn();
     accountWithContext.commit();
 
@@ -40,9 +40,7 @@ export class LogInAccountCommandHandler
       id: accountDomain.getId(),
       username: accountDomain.getUsername(),
       email: accountDomain.getEmail(),
-      roles: accountDomain
-        .getRoles()
-        .map((role) => ({ id: role.getId(), name: role.getName() })),
+      roles: accountDomain.getRoles().map((role) => role.getId()),
     };
     return this.authService.generateTokens(tokenPayload);
   }
