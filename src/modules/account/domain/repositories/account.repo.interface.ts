3c85c39b8638f12ff4927d42620b0e0b38c7442d
crays26 <dinhkiet2610@gmail.com>
2025-12-24 23:09:43 +0700

    add role repository interface in domain in account module

--- src/modules/account/domain/repositories/account.repo.interface.ts
+++ src/modules/account/domain/repositories/account.repo.interface.ts
@@ -1,9 +1,8 @@
-import { Account } from '../../infrastructure/entities/account.entity';
 import { AccountAggRoot } from '../aggregate-root/account.agg-root';
-import {BaseRepository} from "src/shared/ddd/domain/base/repository.base";
+import { BaseRepository } from 'src/shared/ddd/domain/base/repository.base';
 
 export interface IAccountRepository extends BaseRepository<AccountAggRoot> {
   findByEmail(email: string): Promise<AccountAggRoot | null>;
 }
 
-export const ACCOUNT_REPO = Symbol('ACCOUNT_REPO')
+export const ACCOUNT_REPO = Symbol('ACCOUNT_REPO');
---
+++ src/modules/account/domain/repositories/role.repo.interface.ts
@@ -0,0 +1,15 @@
+import { BaseRepository } from 'src/shared/ddd/domain/base/repository.base';
+import { RoleAggRoot } from '../aggregate-root/role.agg-root';
+import { RoleName } from 'src/shared/auth/types/role.type';
+
+export interface IRoleRepository extends BaseRepository<RoleAggRoot> {
+  findById(id: string): Promise<RoleAggRoot | null>;
+  findByName(name: RoleName): Promise<RoleAggRoot | null>;
+  findByIds(ids: string[]): Promise<RoleAggRoot[]>;
+}
+
+export const ROLE_REPO = Symbol('ROLE_REPO');
+
+
+
+
