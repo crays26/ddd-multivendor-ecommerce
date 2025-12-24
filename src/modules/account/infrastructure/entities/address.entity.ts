d2dfe7bde912536bc78d3b8aaf3fe62e77759827
crays26 <dinhkiet2610@gmail.com>
2025-12-24 23:07:43 +0700

    add address persistence entity in infrastructure in account module

---
+++ src/modules/account/infrastructure/entities/address.entity.ts
@@ -0,0 +1,44 @@
+import { Entity, ManyToOne, Opt, PrimaryKey, Property } from '@mikro-orm/core';
+import { AccountEntity } from 'src/modules/account/infrastructure/entities/account.entity';
+
+@Entity({ tableName: 'address' })
+export class AddressEntity {
+  @PrimaryKey({ type: 'uuid' })
+  id!: string;
+
+  @Property()
+  fullName!: string;
+
+  @Property()
+  phoneNumber!: string;
+
+  @Property()
+  street!: string;
+
+  @Property({ nullable: true })
+  unit?: string;
+
+  @Property({ nullable: true })
+  ward?: string;
+
+  @Property()
+  city!: string;
+
+  @Property()
+  region!: string;
+
+  @Property()
+  postalCode!: string;
+
+  @Property()
+  countryCode!: string;
+
+  @Property({ default: false })
+  isDefault!: boolean & Opt;
+
+  @Property()
+  label!: string;
+
+  @ManyToOne(() => AccountEntity)
+  account!: AccountEntity;
+}
