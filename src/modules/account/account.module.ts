9b3b0a650842312429453e3e0c0e107b70217496
crays26 <dinhkiet2610@gmail.com>
2025-12-24 23:11:30 +0700

    update account.module.ts

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -4,6 +4,7 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { AuthController } from './presentation/controllers/account.controller';
 import { AccountRepository } from './infrastructure/repositories/account.repo';
 import { RoleEntity } from './infrastructure/entities/role.entity';
+import { RoleRepository } from './infrastructure/repositories/role.repo';
 import { SignUpAccountCommandHandler } from './application/commands/sign-up-account/handler';
 import { LogInAccountCommandHandler } from './application/commands/log-in-account/handler';
 import { AuthService } from 'src/shared/auth/auth.service';
@@ -13,6 +14,7 @@ import { UnitOfWork } from 'src/shared/ddd/infrastructure/unit-of-work/unit-of-w
 import { UNIT_OF_WORK } from 'src/shared/ddd/infrastructure/unit-of-work/unit-of-work.interface';
 import { UnitOfWorkModule } from 'src/shared/ddd/infrastructure/unit-of-work/unit-of-work.module';
 import { ACCOUNT_REPO } from './domain/repositories/account.repo.interface';
+import { ROLE_REPO } from './domain/repositories/role.repo.interface';
 import { AddCustomerRoleEventHandler } from 'src/modules/account/application/event-handlers/add-customer-role.event-handler';
 import { AddRoleToAccountCommandHandler } from 'src/modules/account/application/commands/add-role-to-account/handler';
 import { AccountReadRepository } from 'src/modules/account/infrastructure/repositories/account.read.repo';
@@ -42,6 +44,10 @@ const EventHandlers = [AddCustomerRoleEventHandler, AddVendorRoleEventHandler];
     {
       provide: ACCOUNT_REPO,
       useClass: AccountRepository,
+    },
+    {
+      provide: ROLE_REPO,
+      useClass: RoleRepository,
     },
     AccountReadRepository,
     AuthService,
