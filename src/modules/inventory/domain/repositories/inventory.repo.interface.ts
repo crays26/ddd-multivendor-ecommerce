fab1aad51cc4c009402f2e81dcefb2d2bdc6db8f
crays26 <dinhkiet2610@gmail.com>
2026-01-29 23:08:38 +0700

    update inventory repo interface in domain

--- src/modules/inventory/domain/repositories/inventory.repo.interface.ts
+++ src/modules/inventory/domain/repositories/inventory.repo.interface.ts
@@ -4,6 +4,10 @@ import { InventoryAggRoot } from '../aggregate-roots/inventory.agg-root';
 export interface IInventoryRepository extends BaseRepository<InventoryAggRoot> {
   findByVariantId(variantId: string): Promise<InventoryAggRoot | null>;
   findByVariantIds(variantIds: string[]): Promise<InventoryAggRoot[]>;
+  confirmReservation(variantId: string, amount: number): Promise<void>;
+  reserveStock(variantId: string, amount: number): Promise<void>;
+  releaseStock(variantId: string, amount: number): Promise<void>;
+  restock(variantId: string, amount: number): Promise<void>;
 }
 
 export const INVENTORY_REPO = Symbol('INVENTORY_REPO');
