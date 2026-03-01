03cda2c2ed507562e8a960c5f38bad93acb6975a
crays26 <dinhkiet2610@gmail.com>
2026-03-01 21:09:11 +0700

    update remaining of order domain

--- src/modules/order/domain/repositories/order.repo.interface.ts
+++ src/modules/order/domain/repositories/order.repo.interface.ts
@@ -2,7 +2,7 @@ import { BaseRepository } from 'src/shared/ddd/domain/base/repository.base';
 import { OrderAggRoot } from 'src/modules/order/domain/aggregate-roots/order.agg-root';
 
 export interface IOrderRepository extends BaseRepository<OrderAggRoot> {
-  findByOrderGroupId(orderGroupId: string): Promise<OrderAggRoot[]>;
+  findByCheckoutId(checkoutId: string): Promise<OrderAggRoot[]>;
   bulkInsert(aggregates: OrderAggRoot[]): Promise<void>;
 }
 
