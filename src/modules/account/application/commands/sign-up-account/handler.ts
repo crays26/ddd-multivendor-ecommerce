d96fe4add1efb26ccb7d6790403027b66328723a
crays26 <dinhkiet2610@gmail.com>
2026-03-22 20:29:42 +0700

    update sign up command handler in account module

--- src/modules/account/application/commands/sign-up-account/handler.ts
+++ src/modules/account/application/commands/sign-up-account/handler.ts
@@ -4,6 +4,7 @@ import { AccountAggRoot } from 'src/modules/account/domain/aggregate-root/accoun
 import { AuthService } from 'src/shared/auth/auth.service';
 import { ConflictException, Inject, NotFoundException } from '@nestjs/common';
 import { Transactional } from '@mikro-orm/core';
+import { instanceToPlain } from 'class-transformer';
 import {
   ACCOUNT_REPO,
   IAccountRepository,
@@ -14,6 +15,10 @@ import {
 } from 'src/modules/account/domain/repositories/role.repo.interface';
 import { RoleName } from 'src/shared/auth/types/role.type';
 import { RoleIdVO } from 'src/modules/account/domain/value-objects/role-id.vo';
+import { AccountSignedUpEvent } from 'src/modules/account/domain/events/account-signed-up.event';
+import { OutboxRepository } from 'src/shared/ddd/infrastructure/outbox/outbox.repo';
+import { Status } from 'src/shared/ddd/infrastructure/outbox/outbox.entity';
+import { v7 as uuidV7 } from 'uuid';
 
 @CommandHandler(SignUpAccountCommand)
 export class SignUpAccountCommandHandler
@@ -25,6 +30,7 @@ export class SignUpAccountCommandHandler
     @Inject(ROLE_REPO)
     private readonly roleRepo: IRoleRepository,
     private readonly authService: AuthService,
+    private readonly outboxRepository: OutboxRepository,
   ) {}
 
   @Transactional()
@@ -49,19 +55,17 @@ export class SignUpAccountCommandHandler
 
     await this.accountRepo.insert(accountDomainEntity);
 
-    // Save AccountSignedUpEvent to outbox
-    // const signedUpEvent = new AccountSignedUpEvent(
-    //   accountDomainEntity.getId(),
-    //   command.data.email,
-    // );
+    const signedUpEvent = new AccountSignedUpEvent(
+      accountDomainEntity.getId(),
+      command.data.email,
+    );
 
-    // await this.outboxRepository.save({
-    //   id: uuidV7(),
-    //   name: signedUpEvent.constructor.name,
-    //   payload: signedUpEvent,
-    //   status: Status.PENDING,
-    //   createdAt: new Date(),
-    // });
+    await this.outboxRepository.save({
+      id: uuidV7(),
+      name: signedUpEvent.constructor.name,
+      payload: instanceToPlain(signedUpEvent),
+      status: Status.PENDING,
+    });
 
     return accountDomainEntity.getId();
   }
